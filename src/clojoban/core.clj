798334085b417cbd1dc88a517cf27f6158900ce1
alvaro-cuesta <alvaro-cuesta@GitHub>
2012-06-10 22:24:43 +0200

    More README tests

--- src/clojoban/core.clj
+++ src/clojoban/core.clj
@@ -19,7 +19,7 @@
       ((game-controller action identity) session)))) ; HACK! game-controller
 
 (defroutes clojoban
-  (GET "/game" []
+  (GET "/game.png" []
        (fn [{:keys [headers session]}]
          (let [new-session (update-session session (headers "referer"))]
            (-> (response (piped-input-stream (generate-image new-session)))
